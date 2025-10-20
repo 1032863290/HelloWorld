2e7a461e80bd64acf064fafad41e3f8884976c30
ywj <1032863290@qq.com>
2025-10-20 16:33:23 +0800

    实现了sort排序算法

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
 void sort(int arr[],int size){
-
+	int tmp;
+	for(int i = 0;i < size - 1;i++){
+		for(int j = i + 1;j < size - i - 1;j++){
+			if(arr[i] > arr[j]){
+				tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
 }
 
 int main(){
