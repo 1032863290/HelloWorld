31ef1719ae65ac88dc815362a7fdbef7cb9a6498
JSUT-ywj <1032863290@qq.com>
2025-10-21 09:32:27 +0800

    阿亮优化了冒泡排序算法

--- main.cpp
+++ main.cpp
@@ -2,14 +2,18 @@
 
 void sort(int arr[],int size){
 	int tmp;
+	bool flag = false;
 	for(int i = 0;i < size - 1;i++){
 		for(int j = i + 1;j < size - i - 1;j++){
 			if(arr[i] > arr[j]){
 				tmp = arr[i];
 				arr[i] = arr[j];
 				arr[j] = tmp;
+				flag = true;
 			}
 		}
+		// 阿亮优化了冒泡排序
+		if(!flag) break;
 	}
 }
 
